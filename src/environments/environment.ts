5d741c974bbf259a2631d5ebc3152e6dd96126fc
rushikesh <rushikesh@girdmail.com>
2025-04-19 16:00:19 +0530

    added git and env file app 19-04-25

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// import { GET_DOMAIN } from '@gird/ui';
+export const FEATURE_NAME = 'generalPolicyFeature';
+
+// let domain = GET_DOMAIN();
+
+// if (window.location.href.includes('localhost')) domain = 'officegird.com';
+
+export const environment = {
+    // apiUrl: 'http://localhost:5000/api/',
+    apiUrl: 'https://job-tech-api.vercel.app/api/'
+};
